39c27df58b672d85754e5230d72269022ea3ee08
Hari Maurya <4748625+harimaurya@users.noreply.github.com>
2025-08-19 12:40:59 +1200

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ChatConfigProvider } from "@/store/chat-config";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
           {children}
         </ChatConfigProvider>
       </body>
+      <GoogleAnalytics gaId="G-GP46SFF7M7" />
     </html>
   );
 }
